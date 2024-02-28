e2ca3e544aad373612d1ad10657afaa06dd3517c
noah-s-braun <imaginoah@icloud.com>
2024-02-27 21:35:16 -0600

    edited homepage and menu to match careers page

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
   ////SHADOWBOX
     //toggle '.active' on elements with '.shadowbox'
     $('.shadowbox').on('click', function() {
-        const image = $('.shadowbox img');
+        const image = $(this);
+        console.log(image.width());
         var standee = $('<div>');
         standee.css('width', image.width());
         standee.css('height', image.height());
         standee.addClass('temporary');
-        console.log(`${standee.width()}, ${standee.height()}`);
-
-        //switch shadowbox
-        $(this).toggleClass('active');
 
         //remove temporary elements
         $('div.temporary').detach();
+
+        //switch shadowbox
+        $(this).toggleClass('active');
 
         //if shadowbox is active, insert standee
         if($(this).hasClass('active')) {
