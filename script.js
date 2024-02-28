0bd75b4de1d4c81741fec605e2d74fd29b4f74aa
noah-s-braun <imaginoah@icloud.com>
2024-02-27 21:00:30 -0600

    added standee to shadowbox

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
 
+  ////SHADOWBOX
     //toggle '.active' on elements with '.shadowbox'
     $('.shadowbox').on('click', function() {
+        const image = $('.shadowbox img');
+        var standee = $('<div>');
+        standee.css('width', image.width());
+        standee.css('height', image.height());
+        standee.addClass('temporary');
+        console.log(`${standee.width()}, ${standee.height()}`);
+
+        //switch shadowbox
         $(this).toggleClass('active');
+
+        //remove temporary elements
+        $('div.temporary').detach();
+
+        //if shadowbox is active, insert standee
+        if($(this).hasClass('active')) {
+            $(this).before(standee);
+        }
     });
 
 });
